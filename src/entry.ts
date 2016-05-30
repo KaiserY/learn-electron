906a1b41992b0dda1c312f0cfe4cac6667127782
yueyang <yueyang@sursen.net>
2016-05-30 17:59:20 +0800

    add codemirror

--- src/entry.ts
+++ src/entry.ts
@@ -1,9 +1,14 @@
-require("../node_modules/material-design-lite/material.min.js")
-require("../node_modules/jquery/dist/jquery.js")
+require("../node_modules/material-design-lite/material.min.js");
+require("../node_modules/jquery/dist/jquery.js");
 
-require("./renderer.ts")
+require("./renderer.ts");
 
-require("../node_modules/material-design-icons/iconfont/material-icons.css")
-require("./roboto.css")
-require("./vendor/material.min.css")
-require("./app.css")
+require("../node_modules/material-design-icons/iconfont/material-icons.css");
+require("./roboto.css");
+require("./vendor/material.min.css");
+
+require("../node_modules/codemirror/lib/codemirror.js");
+require("../node_modules/codemirror/lib/codemirror.css");
+require("../node_modules/codemirror/mode/perl/perl.js");
+
+require("./app.css");
--- src/renderer.ts
+++ src/renderer.ts
@@ -2,32 +2,43 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-import * as fs from 'fs'
-import * as os from 'os'
-var sudo = require('electron-sudo')
+import * as fs from 'fs';
+import * as os from 'os';
+import * as CodeMirror from 'codemirror'
+var sudo = require('electron-sudo');
 
-let hostsPath: string = '/etc/hosts'
+let hostsPath: string = '/etc/hosts';
 
 switch (os.platform()) {
-    case 'win32':
-        hostsPath = 'C:/Windows/System32/drivers/etc/hosts'
-        break
-    case 'linux':
-        hostsPath = '/etc/hosts'
-        break
-}
+  case 'win32':
+    hostsPath = 'C:/Windows/System32/drivers/etc/hosts';
+    break;
+  case 'linux':
+    hostsPath = '/etc/hosts';
+    break;
+};
+
+let textarea = <HTMLTextAreaElement>document.getElementById("app-editor");
+let appCodeMirror: CodeMirror.EditorFromTextArea = null;
 
 fs.readFile(hostsPath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error('error: ' + err)
-    } else {
-        console.log(data)
-    }
-})
+  var text;
+  if (err) {
+    text = 'error: ' + err;
+    console.error(text);
+  } else {
+    text = data;
+    textarea.innerText = text;
+  };
+  appCodeMirror = CodeMirror.fromTextArea(textarea, {
+    mode: "Perl",
+    lineNumbers: true
+  });
+});
 
 // var options = {
 //     name: 'test'
-// }
+// };
 //
 // sudo.exec('echo hello', options, (error) => {
 //     if (error) {
@@ -35,4 +46,4 @@ fs.readFile(hostsPath, 'utf-8', (err, data) => {
 //     } else {
 //         console.log('sudo done!')
 //     }
-// })
+// });
