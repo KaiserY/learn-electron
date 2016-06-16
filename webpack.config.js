83bc331b448eabe64dd99f2800974b8abc9f7562
yueyang <yueyang@sursen.net>
2016-06-16 10:04:54 +0800

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,49 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: {
-    index: "./src/entry.ts"
+    index: './src/entry.ts'
   },
-  target: "electron",
-  devtool: "source-map",
+  target: 'electron',
+  devtool: 'source-map',
   output: {
-    path: path.join(__dirname, "app/public"),
-    filename: "[name].bundle.js"
+    path: path.join(__dirname, 'app/public'),
+    filename: '[name].bundle.js'
   },
   resolve: {
-    extensions: ["", ".js", ".ts"]
+    extensions: ['', '.js', '.ts']
   },
   module: {
     loaders: [{
       test: /\.css$/,
-      loader: "style!css"
+      loader: 'style!css'
     }, {
       test: /\.ts$/,
-      loader: "awesome-typescript-loader",
+      loader: 'awesome-typescript-loader',
       exclude: [/\.(spec|e2e)\.ts$/]
     }, {
       test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
-      loader: "url-loader?limit=30000&name=[name].[ext]"
+      loader: 'url-loader?limit=30000&name=[name].[ext]'
     }]
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/index.html",
-    chunks: ["index"]
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunks: ['index']
+    }),
+    new CopyWebpackPlugin([{
+      from: 'src/main.js'
+    }])
+  ],
   externals: [
     nodeExternals({
       whitelist: [
-        "jquery",
+        'jquery',
         /^material-design/,
-        "codemirror"
+        'codemirror'
       ]
     })
   ]
